8cabf3110b34ae69da9efb53004d93bf3ae17774
Arseniy <khudyakov.03@bk.ru>
2025-12-21 22:00:54 +0300

    Init commit

---
+++ src/main/kotlin/org/bazar/bazar/space/BazarSpaceApplication.kt
@@ -0,0 +1,11 @@
+package org.bazar.bazar.space
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BazarSpaceApplication
+
+fun main(args: Array<String>) {
+    runApplication<BazarSpaceApplication>(*args)
+}
---
+++ src/test/kotlin/org/bazar/bazar/space/BazarSpaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bazar.bazar.space
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BazarSpaceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
