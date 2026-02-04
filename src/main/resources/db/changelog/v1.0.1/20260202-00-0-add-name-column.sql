90381d1f04b175cd67888d0dd328cb71ce978af5
Arseniy <102667061+AsterYnG@users.noreply.github.com>
2026-02-04 19:42:13 +0300

    BZR-38 added not null + default on name column (#12)

--- src/main/resources/db/changelog/v1.0.1/20260202-00-0-add-name-column.sql
+++ src/main/resources/db/changelog/v1.0.1/20260202-00-0-add-name-column.sql
@@ -2,6 +2,7 @@
 --changeset AsterYng:1
 --description add name column
 alter table space
-add column name varchar(128);
+add column name varchar(128) not null default 'поменяй меня пж';
+
 
 comment on column space.name IS 'Space name';
